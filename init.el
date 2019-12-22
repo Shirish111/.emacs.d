dd6bcfee51347f93527bdc6eeee786c468f82089
shirish111 <shirishtogarla533@gmail.com>
2019-12-22 16:38:23 +0530

    Add use-package

--- init.el
+++ init.el
@@ -2,3 +2,13 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;;; Use Package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
